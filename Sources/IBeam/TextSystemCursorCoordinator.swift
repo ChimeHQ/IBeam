1e8aca70b021f29d18dc99b722834e21a98209fa
Matt <85322+mattmassicotte@users.noreply.github.com>
2024-12-16 14:05:11 -0500

    Fix whitespace

--- Sources/IBeam/TextSystemCursorCoordinator.swift
+++ Sources/IBeam/TextSystemCursorCoordinator.swift
@@ -4,84 +4,84 @@ import AppKit
 @available(macOS 14.0, *)
 @MainActor
 public final class TextSystemCursorCoordinator<System: TextSystem> where System.TextRange == NSRange {
-    typealias CursorState = MultiCursorState<System>
+	typealias CursorState = MultiCursorState<System>
 
-    private let textView: NSTextView
-    private let indicatorState: TextViewIndicatorState
-    private let cursorState: CursorState
-    private var selectionNotification: NSObjectProtocol?
-    private var viewCursor: Cursor<CursorState.TextRange>
-    private var mutatingSelection = false
+	private let textView: NSTextView
+	private let indicatorState: TextViewIndicatorState
+	private let cursorState: CursorState
+	private var selectionNotification: NSObjectProtocol?
+	private var viewCursor: Cursor<CursorState.TextRange>
+	private var mutatingSelection = false
 
-    public init(textView: NSTextView, system: System) {
-        self.textView = textView
+	public init(textView: NSTextView, system: System) {
+		self.textView = textView
 
-        self.viewCursor = system.initialCursor()!
-        self.indicatorState = TextViewIndicatorState(textView: textView, viewCursorId: viewCursor.id)
-        indicatorState.boundingRectProvider = { system.boundingRect(for: $0)?.integral }
+		self.viewCursor = system.initialCursor()!
+		self.indicatorState = TextViewIndicatorState(textView: textView, viewCursorId: viewCursor.id)
+		indicatorState.boundingRectProvider = { system.boundingRect(for: $0)?.integral }
 
-        self.cursorState = CursorState(
-            cursors: [viewCursor],
-            system: system
-        )
+		self.cursorState = CursorState(
+			cursors: [viewCursor],
+			system: system
+		)
 
-        self.selectionNotification = NotificationCenter.default.addObserver(
-            forName: NSTextView.didChangeSelectionNotification,
-            object: textView,
-            queue: .main,
-            using: { [weak self] _ in
-                MainActor.assumeIsolated {
-                    self?.selectionChanged()
-                }
-            }
-        )
+		self.selectionNotification = NotificationCenter.default.addObserver(
+			forName: NSTextView.didChangeSelectionNotification,
+			object: textView,
+			queue: .main,
+			using: { [weak self] _ in
+				MainActor.assumeIsolated {
+					self?.selectionChanged()
+				}
+			}
+		)
 
-        cursorState.cursorsChanged = { [weak self] in self?.cursorsUpdated(added: $0, deleted: $1, changed: $2) }
-    }
+		cursorState.cursorsChanged = { [weak self] in self?.cursorsUpdated(added: $0, deleted: $1, changed: $2) }
+	}
 
-    private func selectionChanged() {
-        if mutatingSelection {
-            return
-        }
+	private func selectionChanged() {
+		if mutatingSelection {
+			return
+		}
 
-        viewCursor.textRange = textView.selectedRange()
+		viewCursor.textRange = textView.selectedRange()
 
-        cursorState.mutateCursors(with: .resetToSingle(viewCursor))
-    }
+		cursorState.mutateCursors(with: .resetToSingle(viewCursor))
+	}
 
-    private func cursorsUpdated(added: Set<UUID>, deleted: Set<UUID>, changed: Set<UUID>) {
-        for id in deleted {
-            indicatorState.removeIndicator(with: id)
-        }
+	private func cursorsUpdated(added: Set<UUID>, deleted: Set<UUID>, changed: Set<UUID>) {
+		for id in deleted {
+			indicatorState.removeIndicator(with: id)
+		}
 
-        let existing = added.union(changed)
+		let existing = added.union(changed)
 
-        // this is inefficient
-        let existingCursors = cursorState.cursors.filter({ existing.contains($0.id) })
+		// this is inefficient
+		let existingCursors = cursorState.cursors.filter({ existing.contains($0.id) })
 
-        mutatingSelection = true
+		mutatingSelection = true
 
-        for cursor in existingCursors {
-            indicatorState.updateIndictor(with: cursor.textRange, for: cursor.id)
-        }
+		for cursor in existingCursors {
+			indicatorState.updateIndictor(with: cursor.textRange, for: cursor.id)
+		}
 
-        mutatingSelection = false
-    }
+		mutatingSelection = false
+	}
 
-    public func processOperation(_ operation: InputOperation) {
-        mutatingSelection = true
-        cursorState.apply(operation)
-        mutatingSelection = false
+	public func processOperation(_ operation: InputOperation) {
+		mutatingSelection = true
+		cursorState.apply(operation)
+		mutatingSelection = false
 
-        // if we have removed all cursors, process a selection change to
-        // restore our state
-        if cursorState.cursors.isEmpty {
-            selectionChanged()
-        }
-    }
+		// if we have removed all cursors, process a selection change to
+		// restore our state
+		if cursorState.cursors.isEmpty {
+			selectionChanged()
+		}
+	}
 
-    public func mutateCursors(with operation: CursorOperation<NSRange>) {
-        cursorState.mutateCursors(with: operation)
-    }
+	public func mutateCursors(with operation: CursorOperation<NSRange>) {
+		cursorState.mutateCursors(with: operation)
+	}
 }
 #endif
